613a480f303071909c354d2e3945ea43a2031a49
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-08-29 11:55:49 -0300

    established variables

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = () =>{
+  const song = document.querySelector(".song")
+  const play = document.querySelector(".play") 
+  const outline = document.querySelector(".moving-outline circle")
+  const video = document.querySelector(".vid-container video")  
+
+
+  const Sounds = document.querySelectorAll("sound-picker button")
+
+  const timeDisplay = document.querySelector(".time-display")
+
+  const outlineLength = outline.getTotalLength()
+
+  console.log(outlineLength)
+}
+app()
